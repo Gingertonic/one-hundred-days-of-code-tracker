aea8e9944cd160f43f5db8efb0f5a03a0ebfc68f
Gingertonic <bethmschofield@gmail.com>
2018-11-16 19:13:32 +0000

    outline project handle on click demo

--- one-hundred-days-of-code-tracker-react-frontend/src/containers/DaySquare.js
+++ one-hundred-days-of-code-tracker-react-frontend/src/containers/DaySquare.js
@@ -1,10 +1,14 @@
 import React, {Component} from 'react';
 
 class DaySquare extends Component {
+  handleOnClick = () => {
+    alert(`This is project ${this.props.project.name}`)
+  }
+
   render(){
     return(
       <div className ="day-square">
-        <img className ="day-square-img" src={this.props.project.img} alt="project image" />
+        <img className ="day-square-img" src={this.props.project.img} alt="project image" onClick={this.handleOnClick}/>
       </div>
     )
   }
