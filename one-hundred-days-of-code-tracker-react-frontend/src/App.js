8914a131a18ac92b37960007582bc15c7b1a419a
Gingertonic <bethmschofield@gmail.com>
2018-11-16 21:20:51 +0000

    update project seed data

--- one-hundred-days-of-code-tracker-react-frontend/src/App.js
+++ one-hundred-days-of-code-tracker-react-frontend/src/App.js
@@ -1,19 +1,11 @@
 import React, { Component } from 'react';
 import DaySquare from './containers/DaySquare'
+import {projects} from './projects'
 import './App.css';
 
 class App extends Component {
 
   render() {
-    const projects = new Array(100).fill({
-      day: 1,
-      name: "AI Chatbot",
-      img: "https://media.giphy.com/media/1ircViuXgDKJXMlNVS/giphy.gif",
-      topic: "Bits and Pieces",
-      linkToCode: "https://github.com/Gingertonic/one-hundred-days-of-code-tracker",
-      linkToDemo: "https://media.giphy.com/media/1ircViuXgDKJXMlNVS/giphy.gif"
-    })
-
     const mapProjects =  projects.map(p => <DaySquare project={p}/>)
     return (
       <div className="App">
--- one-hundred-days-of-code-tracker-react-frontend/src/components/ProjectInfo.js
+++ one-hundred-days-of-code-tracker-react-frontend/src/components/ProjectInfo.js
@@ -3,12 +3,13 @@ import React from 'react';
 const ProjectInfo = props => {
   return (
     <div className="project-info">
-      <h1>Day {props.project.day}</h1>
-      <p>{props.project.name}</p>
+      <h1>{props.project.name}</h1>
+      <p>Day {props.project.day}</p>
       <p>{props.project.topic}</p>
+      <img className="focus-image" src={props.project.img} alt={props.project.name}/>
       <ul>
-      <li><a href={props.project.linkToCode}>See the Code</a></li>
-      <li><a href={props.project.linkToDemo}>See the Demo</a></li>
+        <li><a href={props.project.linkToCode}>See the Code</a></li>
+        <li><a href={props.project.linkToDemo}>See it in Action</a></li>
       </ul>
     </div>
   )
